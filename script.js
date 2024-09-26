38c9039909a207803679a486b34d6e40ee41fab2
Tanishq Varshney <tanishqanivarshney@gmail.com>
2024-09-26 15:52:37 +0530

    Git commit.

---
+++ script.js
@@ -0,0 +1,11 @@
+let signInCard = document.createElement('div');
+
+signInCard.id = "signIn";
+
+signInCard.style.height = "10rem";
+signInCard.style.height = "10rem";
+
+let body = document.querySelector('body');
+
+body.appendChild(signInCard);
+
