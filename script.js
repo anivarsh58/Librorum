5e2f4974f93c69d23b14de14466727952aa051c0
Tanishq Varshney <tanishqanivarshney@gmail.com>
2024-09-26 16:04:45 +0530

    Add script.js and Sign-Up card.

--- script.js
+++ script.js
@@ -17,7 +17,28 @@ let signInCard = document.createElement("div");
         }
       });
 
-    let body = document.querySelector("body");
+let body = document.querySelector("body");
 
-    body.appendChild(signInCard);
+body.appendChild(signInCard);
+
+let signUpCard = document.createElement("div");
+
+    signUpCard.id = "signUp";
+
+    signUpCard.style.width = "21rem";
+    signUpCard.style.height = "14rem";
+    signUpCard.style.backgroundColor = "#F5F5F5";
+    signUpCard.style.display = "none";
+
+    document
+      .querySelector("#sign-up")
+      .addEventListener("click", function (event) {
+        console.log("Clicked.");
+        if (signInCard.style.display == "none") {
+          console.log("Inside clicked.");
+          signInCard.style.display = "block";
+        }
+      });
+
+body.appendChild(signUpCard);
 
