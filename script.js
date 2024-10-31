cce1b5baf309b0ac88f5877757d8a6250fc470d5
Tanishq Varshney <tanishqanivarshney@gmail.com>
2024-10-31 12:51:27 +0530

    Add sign-in  sign-up forms.

--- script.js
+++ script.js
@@ -1,44 +1,25 @@
-let signInCard = document.createElement("div");
+signInCard = document.querySelector("#signIn")
+signUpCard = document.querySelector('#signUp')
 
-    signInCard.id = "signIn";
+signInCard.style.display = "none";
+signUpCard.style.display = "none";
 
-    signInCard.style.width = "21rem";
-    signInCard.style.height = "14rem";
-    signInCard.style.backgroundColor = "#F5F5F5";
-    signInCard.style.display = "none";
+document
+    .querySelector("#sign-in")
+    .addEventListener("click", function (event) {
+    console.log("Clicked.");
+    if (signInCard.style.display == "none") {
+        console.log("Inside clicked.");
+        signInCard.style.display = "block";
+    }
+    });
 
-    document
-      .querySelector("#sign-in")
-      .addEventListener("click", function (event) {
-        console.log("Clicked.");
-        if (signInCard.style.display == "none") {
-          console.log("Inside clicked.");
-          signInCard.style.display = "block";
-        }
-      });
-
-let body = document.querySelector("body");
-
-body.appendChild(signInCard);
-
-let signUpCard = document.createElement("div");
-
-    signUpCard.id = "signUp";
-
-    signUpCard.style.width = "21rem";
-    signUpCard.style.height = "14rem";
-    signUpCard.style.backgroundColor = "#F5F5F5";
-    signUpCard.style.display = "none";
-
-    document
-      .querySelector("#sign-up")
-      .addEventListener("click", function (event) {
-        console.log("Clicked.");
-        if (signInCard.style.display == "none") {
-          console.log("Inside clicked.");
-          signInCard.style.display = "block";
-        }
-      });
-
-body.appendChild(signUpCard);
-
+document
+    .querySelector("#sign-up")
+    .addEventListener("click", function (event) {
+    console.log("Clicked.");
+    if (signUpCard.style.display == "none") {
+        console.log("Inside clicked.");
+        signUpCard.style.display = "block";
+    }
+    });
